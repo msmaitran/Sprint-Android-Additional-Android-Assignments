efe396a2225303b7218db3ffe74c8d29800d8709
msmaitran <itsmaisn@gmail.com>
2019-10-09 18:00:28 -0700

    Add Navigation Drawer.

---
+++ M03/Assignment/AndroidMenusDesign/app/src/main/java/com/lambdaschool/androidmenusdesign/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.lambdaschool.androidmenusdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private var drawerLayout: DrawerLayout? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Navigation Drawer
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        toolbar.title = title
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.closer_drawer)
+
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+
+            when (menuItem.itemId) {
+                R.id.nav_home -> { Toast.makeText(this, "Goes to Homepage", Toast.LENGTH_LONG).show() }
+                R.id.nav_profile -> { Toast.makeText(this, "Goes to Profile Page", Toast.LENGTH_LONG).show() }
+                R.id.nav_notifications -> { Toast.makeText(this, "Goes to Notifications Page", Toast.LENGTH_LONG).show() }
+            }
+
+            drawerLayout?.closeDrawers()
+
+            true
+        }
+    }
+}
